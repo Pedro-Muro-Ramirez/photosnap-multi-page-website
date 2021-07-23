8ce9d2ec73cb83822da439248573777714d79172
Pedro-Muro-Ramirez <muro285@gmail.com>
2021-07-23 12:23:01 -0500

    added pricing toggle switch

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector(".navbar-toggle");
 const navMenu = document.querySelector(".navbar-menu");
 const navLink = document.querySelectorAll(".navbar-links");
+const toggler = document.querySelector(".toggle-btn");
+const basicPrice = document.querySelector(".basic-cost");
+const proPrice = document.querySelector(".pro-cost");
+const businessPrice = document.querySelector(".business-cost");
+const frequency1 = document.querySelector(".cost-frequency1");
+const frequency2 = document.querySelector(".cost-frequency2");
+const frequency3 = document.querySelector(".cost-frequency3");
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -15,3 +22,21 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+toggler.addEventListener("click", () => {
+  if (toggler.checked) {
+    basicPrice.innerHTML = "$190.00";
+    proPrice.innerHTML = "$390.00";
+    businessPrice.innerHTML = "$990.00";
+    frequency1.innerHTML = "per year";
+    frequency2.innerHTML = "per year";
+    frequency3.innerHTML = "per year";
+  } else {
+    basicPrice.innerHTML = "$19.00";
+    proPrice.innerHTML = "$39.00";
+    businessPrice.innerHTML = "$99.00";
+    frequency1.innerHTML = "per month";
+    frequency2.innerHTML = "per month";
+    frequency3.innerHTML = "per month";
+  }
+});
